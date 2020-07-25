b8689e69c020d1baf6e3fbcfab94fc60f7123aee
SangchoKim <wjdrms1919@gmail.com>
2020-07-25 18:28:35 +0900

    sendMail Function with Nodemailer

--- src/sever.js
+++ src/sever.js
@@ -1,7 +1,11 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
 import {GraphQLServer} from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
+import { sendSecretMail } from '../src/utils'
+dotenv.config();
+
+sendSecretMail("wjdrms1919@gmail.com","123");
 
 const PORT = process.env.PORT || 4000;
 
---
+++ src/utils.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import { adjectives, none } from "./words";
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+dotenv.config();
+
+export const secretGenerator = () => {
+    const randomNumber = Math.floor(Math.random() * adjectives.length);
+    return `${adjectives[randomNumber]}${none[randomNumber]}`
+}
+
+export const sendMail = (email) => {
+    const options = {
+        auth: {
+          api_user: process.env.SENDGRID_USERNAME,
+          api_key: process.env.SENDGRID_PASSWORD
+        }
+      }
+      const client = nodemailer.createTransport(sgTransport(options));
+      return client.sendMail(email);
+};
+ 
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from:"wjdrms1919@gmail.com",
+        to: address,
+        subject: "Login Secret for SantosGram",
+        html: `Hello, your login secret is ${secret}. <br/> Copy paste on the app/website to log in`
+    }
+
+    return sendMail(email);
+}; 
